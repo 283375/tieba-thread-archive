90c91c4c69391875b67c548393a05ed7e16c184d
283375 <bilibili283375@163.com>
2023-04-13 23:39:26 +0800

    wip: introducing base class `LocalArchive`

---
+++ src/tieba_thread_archive/local/archive/archive.py
@@ -0,0 +1,104 @@
+from copy import deepcopy
+from os import PathLike
+from pathlib import Path
+from typing import List, Optional, Set, Union
+
+from ...models.archive import ArchiveOptions, ArchiveThread, ArchiveUpdateInfo
+from ...models.content import ContentAudio, ContentImage, ContentVideo
+from ...models.progress import Progress
+
+
+class LocalArchive:
+    __slots__ = (
+        "_path",
+        "_loaded",
+        "_history",
+        "_history_loaded",
+        "_update_progress",
+        "_archive_options",
+        "_archive_update_info",
+        "_archive_thread",
+        "_images",
+        "_audios",
+        "_videos",
+    )
+
+    _path: Path
+    _loaded: bool
+    _history: List[ArchiveThread]
+    _history_loaded: bool
+    _update_progress: Progress
+    _archive_options: Optional[ArchiveOptions]
+    _archive_update_info: Optional[ArchiveUpdateInfo]
+    _archive_thread: Optional[ArchiveThread]
+    _images: Set[ContentImage]
+    _audios: Set[ContentAudio]
+    _videos: Set[ContentVideo]
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def loaded(self):
+        return self._loaded
+
+    @property
+    def history(self):
+        return deepcopy(self._history)
+
+    @property
+    def history_loaded(self):
+        return self._history_loaded
+
+    @property
+    def update_progress(self):
+        return self._update_progress
+
+    @property
+    def archive_options(self):
+        return self._archive_options
+
+    @property
+    def archive_update_info(self):
+        return self._archive_update_info
+
+    @property
+    def archive_thread(self):
+        return self._archive_thread
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def audios(self):
+        return self._audios
+
+    @property
+    def videos(self):
+        return self._videos
+
+    def __init__(
+        self,
+        path: Union[str, PathLike],
+        *,
+        auto_load: bool = True,
+        auto_load_history: bool = False,
+    ):
+        raise NotImplementedError("LocalArchive is not meant to be used directly.")
+
+    def set_archive_options(self, new_archive_options: ArchiveOptions):
+        raise NotImplementedError("LocalArchive is not meant to be used directly.")
+
+    def dump(self):
+        raise NotImplementedError("LocalArchive is not meant to be used directly.")
+
+    def load(self):
+        raise NotImplementedError("LocalArchive is not meant to be used directly.")
+
+    def load_history(self):
+        raise NotImplementedError("LocalArchive is not meant to be used directly.")
+
+    def update(self, new_archive_thread: ArchiveThread):
+        raise NotImplementedError("LocalArchive is not meant to be used directly.")
--- src/tieba_thread_archive/local/archive/v3/archive.py
+++ src/tieba_thread_archive/local/archive/v3/archive.py
@@ -11,6 +11,7 @@ import yaml
 
 from ....models import *
 from ....models.progress import Progress
+from ..archive import LocalArchive
 from .dump import *
 from .file_structure import *
 from .load import *
@@ -19,68 +20,7 @@ from .validate import *
 __all__ = ("AV3LocalArchive",)
 
 
-class AV3LocalArchive:
-    __slots__ = (
-        "__assets_progress",
-        "__path",
-        "__history",
-        "archive_options",
-        "archive_update_info",
-        "archive_thread",
-        "images",
-        "audios",
-        "videos",
-    )
-
-    __assets_progress: Progress
-
-    __path: Path
-    __history: List[ArchiveThread]
-
-    archive_options: Optional[ArchiveOptions]
-    archive_update_info: Optional[ArchiveUpdateInfo]
-
-    archive_thread: Optional[ArchiveThread]
-
-    images: Set[ContentImage]
-    audios: Set[ContentAudio]
-    videos: Set[ContentVideo]
-
-    def __init__(self, path: Union[str, PathLike]):
-        self.__assets_progress = Progress()
-
-        self.__path = Path(path)
-
-        if not self.__path.exists():
-            raise ValueError(f"Passing in an invalid path {self.__path}.")
-        if (
-            self.__path.exists()
-            and list(self.__path.iterdir())
-            and not av3_validate_path(self.__path)
-        ):
-            raise ValueError(f"{path} is not empty and is not a valid archive.")
-
-        self.__history = []
-        self.archive_options = None
-        self.archive_update_info = None
-
-        self.archive_thread = None
-
-        self.images = set()
-        self.audios = set()
-        self.videos = set()
-
-        if av3_validate_path(self.__path):
-            self.load()
-
-    @property
-    def assets_progress(self):
-        return self.__assets_progress
-
-    @property
-    def path(self):
-        return self.__path
-
+class AV3LocalArchive(LocalArchive):
     @property
     def history_dir(self):
         return self.path / ".history"
@@ -113,14 +53,46 @@ class AV3LocalArchive:
     def assets_file(self):
         return self.path / "assets.yaml"
 
-    @property
-    def history(self):
-        return self.__history
+    def __init__(
+        self,
+        path: Union[str, PathLike],
+        *,
+        auto_load: bool = True,
+        auto_load_history: bool = False,
+    ):
+        self._update_progress = Progress()
 
-    def set_archive_options(self, archive_options: ArchiveOptions):
-        self.archive_options = archive_options
+        self._path = Path(path)
 
-    def __load_history(self):
+        if not self._path.exists():
+            raise ValueError(f"Passing in an invalid path {self._path}.")
+        if (
+            self._path.exists()
+            and list(self._path.iterdir())
+            and not av3_validate_path(self._path)
+        ):
+            raise ValueError(f"{path} is not empty and is not a valid archive.")
+
+        self._history = []
+        self._archive_options = None
+        self._archive_update_info = None
+
+        self._archive_thread = None
+
+        self._images = set()
+        self._audios = set()
+        self._videos = set()
+
+        if av3_validate_path(self._path):
+            if auto_load:
+                self.load()
+            if auto_load_history:
+                self.load_history()
+
+    def set_archive_options(self, new_archive_options):
+        self._archive_options = new_archive_options
+
+    def load_history(self):
         if not (self.history_dir.exists() and self.history_dir.is_dir()):
             # TODO: warning
             return
@@ -140,7 +112,9 @@ class AV3LocalArchive:
                     if archive_history.archive_time in existing_history_times:
                         continue
                     else:
-                        self.__history.append(archive_history)
+                        self._history.append(archive_history)
+
+        self._history_loaded = True
 
     def __dump_history(self):
         if not self.history:
@@ -170,17 +144,20 @@ class AV3LocalArchive:
                 yaml.safe_load(info_rs.read())
             )
 
-            self.archive_options = archive_options
-            self.archive_update_info = archive_update_info
+            self._archive_options = archive_options
+            self._archive_update_info = archive_update_info
 
     def __dump_info(self):
-        if self.archive_thread and self.archive_options and self.archive_update_info:
+        if not isinstance(self._archive_options, ArchiveOptions):
+            raise ValueError("archive_options not set.")
+
+        if self._archive_thread and self._archive_update_info:
             with open(self.info_file, "w", encoding="utf-8") as info_ws:
                 info_ws.write(
                     av3_dump_info_yaml_str(
-                        thread_info=self.archive_thread.thread_info,
-                        archive_options=self.archive_options,
-                        archive_update_info=self.archive_update_info,
+                        thread_info=self._archive_thread.thread_info,
+                        archive_options=self._archive_options,
+                        archive_update_info=self._archive_update_info,
                     )
                 )
         else:
@@ -193,19 +170,19 @@ class AV3LocalArchive:
             archive_thread = av3_load_thread_yaml(yaml.safe_load(thread_rs.read()))
 
             if (
-                self.archive_thread
-                and self.archive_thread.archive_time == archive_thread.archive_time
-                and self.archive_thread.thread_info.id == archive_thread.thread_info.id
+                self._archive_thread
+                and self._archive_thread.archive_time == archive_thread.archive_time
+                and self._archive_thread.thread_info.id == archive_thread.thread_info.id
             ):
                 return
 
-            self.archive_thread = archive_thread
+            self._archive_thread = archive_thread
 
     def __dump_archive_thread(self):
-        if self.archive_thread:
+        if self._archive_thread:
             with open(self.path / "thread.yaml", "w", encoding="utf-8") as thread_ws:
                 thread_ws.write(
-                    av3_dump_thread_yaml_str(archive_thread=self.archive_thread)
+                    av3_dump_thread_yaml_str(archive_thread=self._archive_thread)
                 )
         else:
             raise ValueError(
@@ -216,23 +193,23 @@ class AV3LocalArchive:
         with open(self.assets_file, "r", encoding="utf-8") as assets_rs:
             assets = av3_load_assets_yaml(yaml.safe_load(assets_rs.read()))
 
-            self.images = assets["images"]
-            self.audios = assets["audios"]
-            self.videos = assets["videos"]
+            self._images = assets["images"]
+            self._audios = assets["audios"]
+            self._videos = assets["videos"]
 
     def __dump_assets(self):
         with open(self.assets_file, "w", encoding="utf-8") as assets_ws:
             assets_ws.write(
                 av3_dump_assets_yaml_str(
-                    images=self.images, audios=self.audios, videos=self.videos
+                    images=self._images, audios=self._audios, videos=self._videos
                 )
             )
 
     def load(self):
-        self.__load_history()
         self.__load_info()
         self.__load_archive_thread()
         self.__load_assets()
+        self._loaded = True
 
         # TODO:
         # if info.id != archive_thread.id, warning?
@@ -243,74 +220,74 @@ class AV3LocalArchive:
         self.__dump_archive_thread()
         self.__dump_assets()
 
-    def update_archive_thread(self, new_archive_thread: ArchiveThread):
+    def update(self, new_archive_thread):
         # sourcery skip: extract-method
-        if self.archive_thread is None:
-            self.archive_thread = new_archive_thread
-            self.images = new_archive_thread.images()
-            self.audios = new_archive_thread.audios()
-            self.videos = new_archive_thread.videos()
+        if self._archive_thread is None:
+            self._archive_thread = new_archive_thread
+            self._images = new_archive_thread.images()
+            self._audios = new_archive_thread.audios()
+            self._videos = new_archive_thread.videos()
 
-            self.archive_update_info = ArchiveUpdateInfo(
+            self._archive_update_info = ArchiveUpdateInfo(
                 archive_time=new_archive_thread.archive_time, last_update_time=None
             )
         else:
-            if self.archive_thread == new_archive_thread:
+            if self._archive_thread == new_archive_thread:
                 # TODO: log
                 return
 
-            self.__history.append(deepcopy(self.archive_thread))
+            self._history.append(deepcopy(self._archive_thread))
 
-            self.archive_thread.update(new_archive_thread)
-            self.images = new_archive_thread.images() | self.images
-            self.audios = new_archive_thread.audios() | self.audios
-            self.videos = new_archive_thread.videos() | self.videos
+            self._archive_thread.update(new_archive_thread)
+            self._images = new_archive_thread.images() | self._images
+            self._audios = new_archive_thread.audios() | self._audios
+            self._videos = new_archive_thread.videos() | self._videos
 
-            if self.archive_update_info is None:
+            if self._archive_update_info is None:
                 raise ValueError("TODO: this should not happen")
 
-            self.archive_update_info.last_update_time = new_archive_thread.archive_time
+            self._archive_update_info.last_update_time = new_archive_thread.archive_time
 
     def __executor_task_get_tasks(
         self, session: requests.Session
     ) -> List[Tuple[requests.Session, requests.PreparedRequest, Path]]:
-        if self.archive_options is None:
+        if self._archive_options is None:
             raise ValueError("Need archive_options before downloading assets.")
 
         tasks = []
 
-        if self.archive_options.images and self.images:
+        if self._archive_options.images and self._images:
             tasks.extend(
                 (
                     session,
                     requests.Request("GET", image.origin_src).prepare(),
                     self.images_dir / image.filename,
                 )
-                for image in self.images
+                for image in self._images
             )
-        if self.archive_options.audios and self.audios:
+        if self._archive_options.audios and self._audios:
             tasks.extend(
                 (
                     session,
                     requests.Request("GET", audio.src).prepare(),
                     self.audios_dir / audio.filename,
                 )
-                for audio in self.audios
+                for audio in self._audios
             )
-        if self.archive_options.videos and self.videos:
+        if self._archive_options.videos and self._videos:
             tasks.extend(
                 (
                     session,
                     requests.Request("GET", video.link).prepare(),
                     self.videos_dir / video.filename,
                 )
-                for video in self.videos
+                for video in self._videos
                 if video.link and video.filename
             )
         if (
-            self.archive_options.portraits
-            and self.archive_thread is not None
-            and self.archive_thread.users
+            self._archive_options.portraits
+            and self._archive_thread is not None
+            and self._archive_thread.users
         ):
             tasks.extend(
                 (
@@ -321,7 +298,7 @@ class AV3LocalArchive:
                     ).prepare(),
                     self.portraits_dir / f"{user.id}.jpg",
                 )
-                for user in self.archive_thread.users
+                for user in self._archive_thread.users
             )
 
         return tasks
@@ -347,8 +324,8 @@ class AV3LocalArchive:
             tasks = self.__executor_task_get_tasks(session)
             if tasks:
                 with futures.ThreadPoolExecutor() as executor:
-                    self.__assets_progress.reset()
-                    self.__assets_progress.total_progress = len(tasks)
+                    self._update_progress.reset()
+                    self._update_progress.total_progress = len(tasks)
 
                     executor_tasks = [
                         executor.submit(
@@ -358,4 +335,4 @@ class AV3LocalArchive:
                     ]
 
                     for _ in futures.as_completed(executor_tasks):
-                        self.__assets_progress += 1
+                        self._update_progress += 1
